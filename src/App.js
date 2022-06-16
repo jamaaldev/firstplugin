830951b2ff56972a96d221bfe5b53196c40d6ab3
jamaaldev <kabarojamaal@gmail.com>
2022-06-16 04:18:48 +0100

    First Plugin React 17 Simple Plugin

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+import Dashboard from "./components/Dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="app-title">First Plugin App React</h2>
+      <Dashboard />
+      <Button />
+      <hr />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <>
+      <h1>Welcome Button</h1>
+    </>
+  );
+};
+
+export default Button;
